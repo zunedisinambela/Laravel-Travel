b3999f7826d0b57fc323cbfec666844478409330
zunedisinambela <zunedisinambela@gmail.com>
2021-11-09 20:11:26 +0700

    Report

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,13 +2,20 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Transaction;
+use App\TravelPackage;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        $travel_package = TravelPackage::count();
+        $transaction = Transaction::count();
+        $transaction_pending = Transaction::where('transaction_status', 'PENDING')->count();
+        $transaction_success = Transaction::where('transaction_status', 'SUCCESS')->count();
+
+        return view('pages.admin.dashboard', compact('travel_package', 'transaction', 'transaction_pending', 'transaction_success'));
     }
 }
